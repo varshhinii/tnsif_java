b60f73caec5d641ea1ea3d735837cb0e0e9a5169
Varshhinii <varshiniparamesh2004@gmail.com>
2026-02-17 21:44:07 +0530

    first commit

---
+++ tnsif/src/OopsDemo/Abstraction.java
@@ -0,0 +1,67 @@
+package OopsDemo;
+
+public class Abstraction {
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        MaheshDemo mahesh=new SureshDemo();
+        mahesh.calling();
+        mahesh.AIIntegration();
+        mahesh.SatelliteConn();
+        mahesh.HealthMonitoring();
+
+    }
+
+}
+abstract class MaheshDemo   //2024 abstract class
+{
+    public void calling()   //concrete method
+    {
+        System.out.println("calling");
+    }
+    public abstract void AIIntegration();  //abstract method
+    public abstract void SatelliteConn();
+    public abstract void HealthMonitoring();
+}
+
+abstract class RameshDemo extends  MaheshDemo       //2025
+{
+
+    @Override
+    public void AIIntegration() {
+        System.out.println("Phone is having AI-Integration");
+        
+    }
+
+    @Override
+    public abstract void SatelliteConn();
+
+    @Override
+    public abstract  void HealthMonitoring();
+    
+}
+
+class SureshDemo extends RameshDemo  //2026
+{
+
+    @Override
+    public void SatelliteConn() {
+        
+        System.out.println("Phone is having Satellite Conn");
+    }
+
+    @Override
+    public void HealthMonitoring() {
+        System.out.println("Phone is having HealthMonitoring conn");
+        
+    }
+    
+}
+
+
+
